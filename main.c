a494f034088de1f74ba8056e61ad4bf7bc9b55b4
Minihacker786 <126167747+Minihacker786@users.noreply.github.com>
2023-03-08 00:40:43 +0500

    main.c
    this is the main file of this project

---
+++ main.c
@@ -0,0 +1,161 @@
+#include <stdio.h>=
+int main(){
+	int a,b,c,d,e,f;
+printf("\tchose any number of tool : \n");
+	printf("\n");
+	printf("[1] : WORLD DANGEROUS HACKERS TOOL EXPOSE \n");
+	printf("[2] : Facebook Tool Links\n");
+	printf("[3] : Networking Tool Links\n");
+	printf("[4] : Phishing Tool Links\n");
+	printf("\t\t\t[created by : M_Saim]\n");
+	scanf("%d",&a);
+	//part 1
+	if(a==1){
+		printf("\tAdvance tools\n");
+		printf("[1] : Pish web tool\n");
+		printf("[2] : MITM attack tool\n");
+		printf("[3] : kill shot pentesting framewor\n");
+		scanf("%d",&b);
+		if(b==1)
+		{printf("https://github.com/Cabdulahi/pish");
+			}else if(b==2){
+			printf("https://github.com/websploit/websploit");
+				}else if(b==3){
+					printf("https://github.com/bahaabdelwahed/killshot");
+					}else if(b>=4){
+						printf("You enterd a wrong number ");}
+		}
+		//part 2
+		else if(a==2){
+			printf("\tFacebook Tool Links\n" );
+		printf("[1] :Facebook information gathering\n");
+		printf("[2] :Facebook Toolkit + bots,  dump private data\n");
+		printf("[3] :Facebook cracking tool Fcrack.py\n");
+		printf("[4] :Facebook and yahoo account cloner \n");
+			printf("[5] :Facebook report tool\n");
+			printf("[6] :Facebook BruteFoRce Tool\n");
+			printf("[7] :Facebook hacking ASU\n");
+			printf("[8] :Facebook Downloader\n");
+			printf("[9] :Hack Facebook MBF\n");
+			printf("[10] :Facebook Repot3\n");
+			printf("[11] :Facebook Information Gathering\n");
+			printf("[12] :Facebook Brute with TOR\n");
+			scanf("%d",&c);
+			if(c==1){
+				printf("git clone https://github.com/CiKu370/OSIF.git");
+				}else if(c==2){
+					printf("https://github.com/warifp/FacebookToolkit");
+					}else if(c==3){
+						printf("https://github.com/INDOnimous/FB-Crack-");
+						}else if(c==4){
+						printf("https://gitlab.com/W1nz0N/fyc.git");
+						}else if(c==5){
+							printf("https://github.com/IlayTamvan/Report");
+							}else if(c==6){
+							printf("https://github.com/IAmBlackHacker/Facebook-BruteForce");
+								}else if(c==7){
+								printf("https://github.com/LOoLzeC/ASU");	
+									}else if(c==8){
+									printf("https://github.com/barba99/facebook-spotify-youtube-descargar");
+											}else if(c==9){
+										printf("https://github.com/Rizky-ID/autombf");	
+											}else if(c==10){
+									printf("https://github.com/PangeranAlvins/Repot3");			
+												}else if(c==11){
+								printf("https://github.com/xHak9x/fbi");					}else if(c==12){
+											printf("https://github.com/thelinuxchoice/facebash");			
+														}else if(c>=13){
+															printf("You enterd a wrong number");
+															}
+	}
+	//part 3
+	else if(a==3){
+	printf("\tNetworking Tool \n");
+		printf("[1] : ip camera hacking\n");
+		printf("[2] : Termux Lazyscript tool\n");
+		printf("[3] : TMscanner Tool\n");
+		printf("[4] : Trace location with IP\n");
+		printf("[5] : WPS Wi-Fi hacking tool\n");
+		printf("[6] : Routersploit - vulnerability scanner and attacker \n");
+		printf("[7] : Routersploit - vulnerability scanner and attacker \n");
+		printf("[8] : Local network exploiting tool Zarp\n");
+		printf("[9] : ip tracker,  Device info by link\n");
+		printf("[10] : Ip-Fy IP address information\n");
+		printf("[11] : Wifite Wi-Fi hacking tool\n");
+		//part 4 if else part
+		scanf("%d",&d);
+		if(d==1)
+		{
+			printf("https://github.com/kancotdiq/ipcs");
+		}else if(d==2){
+		printf("https://github.com/TechnicalMujeeb/Termux-Lazyscript");
+			}else if(d==3){
+				printf(" https://github.com/TechnicalMujeeb/TM-scanner");
+				}else if(d==4){
+					printf("https://github.com/Rajkumrdusad/IP-Tracer");
+					}else if(d==5){
+				printf("https://github.com/nxxxu/AutoPixieWps");		
+						}else if(d==6){
+				printf("https://github.com/reverse-shell/routersploit.git");		
+						}else if(d==7){
+							printf("https://github.com/reverse-shell/routersploit.git");}else if(d==8){
+				printf("https://github.com/hatRiot/zar");		
+						}else if(d==9){
+				printf("https://github.com/lucasfarre/ip-tracker");		
+						}else if(d==10){
+				printf("https://github.com/T4P4N/IP-FY.git");		
+						}else if(d==11){
+				printf("https://github.com/derv82/wifite");		
+						}else
+						{
+							printf("you enterd a wrong number");
+						}
+		}
+		//part 4
+		else if(a==4){
+		printf("\tPhishing Tool Links\n");
+			printf("[1] : Modern phishing tool hidden eye\n");
+			printf("[2] : complete phishing tool 32 templates + 1 customizable\n");
+			printf("[3] : social media phishing with shellphish\n");
+			printf("[4] : Advance Phishing OTP Bypass\n");
+			printf("[5] : Paytm Phishing OTP Bypass\n");
+			printf("[6] : UberEats Phishing OTP Bypass\n");
+			printf("[7] : Whats App Phishing\n");
+			printf("[8] : Zomato Phishing\n");
+			printf("[9] : hotstar OTP Bypass\n");
+			printf("[10] : Ola OTP Bypass\n");
+			printf("[11] : Amazon Payment Gateway Phishing\n");
+			//part 4 if else part 
+			scanf("%d",&e);
+			if(e==1){
+				printf("https://github.com/DarkSecDevelopers/HiddenEye");
+				}else if(e==2){
+					printf("https://github.com/thelinuxchoice/blackeye");
+					}else if(e==3){
+					printf("https://github.com/thelinuxchoice/shellphish");
+					}else if(e==4){
+					printf("https://github.com/Ignitetch/AdvPhishing");
+					}else if(e==5){
+					printf("https://github.com/Ignitetch/Paytm-Phishing");
+					}else if(e==6){
+					printf("https://github.com/Ignitetch/UberEats-Phishing");
+					}else if(e==7){
+					printf("https://github.com/Ignitetch/whatsapp-phishing");
+					}else if(e==8){
+					printf("https://github.com/Ignitetch/Zomato-Phishing");
+					}else if(e==9){
+					printf("https://github.com/Ignitetch/Hotstar-otp-bypass");
+					}else if(e==10){
+					printf("https://github.com Ignitetch/ola-otpbypass");
+					}else if(e==11){
+					printf("https://github.com/Ignitetch/Amazon-payment-gateway-phishing");
+					}else if(e>=12){
+					printf("you enterd a wrong number");
+					}
+				}else
+				{
+					printf("You enterd a wrong number");
+				}
+	
+	}
+
